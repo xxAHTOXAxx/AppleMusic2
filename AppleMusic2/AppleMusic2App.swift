1db59df7bfbbf39ded10d662cad6c7fd1b154d9b
xxAHTOXAxx <133795951+xxAHTOXAxx@users.noreply.github.com>
2023-11-26 14:43:24 +0400

    Initial Commit

---
+++ AppleMusic2/AppleMusic2App.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusic2App.swift
+//  AppleMusic2
+//
+//  Created by Антон Исаков on 26.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusic2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleMusic2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleMusic2
+//
+//  Created by Антон Исаков on 26.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
